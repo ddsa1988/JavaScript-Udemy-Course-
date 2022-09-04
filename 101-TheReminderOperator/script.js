593683af2d0da43cd864f7fc4dd3f87046ca7894
Diego Alexandre <ddsa.1988@outlook.com>
2022-09-04 11:26:26 -0300

    commit

---
+++ 101-TheReminderOperator/script.js
@@ -0,0 +1,8 @@
+"strict";
+
+console.log(5 % 2);
+console.log(8 % 3);
+
+const isEven = (number) => number % 2 === 0;
+console.log(isEven(8));
+console.log(isEven(23));
---
+++ 102-NumericSeparators/script.js
@@ -0,0 +1,7 @@
+"use strict";
+
+const diameter = 287_460_000_000;
+console.log(diameter);
+
+const priceCents = 345_99;
+console.log(priceCents);
