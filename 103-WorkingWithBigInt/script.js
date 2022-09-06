45817391c728c3908373253f7b5a6619ae6c31b3
Diego Alexandre <ddsa.1988@outlook.com>
2022-09-06 20:13:36 -0300

    commit

---
+++ 103-WorkingWithBigInt/script.js
@@ -0,0 +1,8 @@
+"use strict";
+
+//Max integer number representation in Javascript
+console.log(2 ** 53 - 1);
+console.log(Number.MAX_SAFE_INTEGER);
+
+//Big Int
+console.log(4855457856465867645876456468768748n);
