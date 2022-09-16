8ef45b868b4dfa28aa5a746791ecf7337ae004ca
Diego Alexandre <ddsa.1988@outlook.com>
2022-09-15 23:15:32 -0300

    commit

--- 117-ImplementingPageNavigation/script.js
+++ 117-ImplementingPageNavigation/script.js
@@ -1,1 +1,19 @@
 "use strict";
+
+// document.querySelectorAll(".nav__link").forEach(function (element) {
+//     element.addEventListener("click", function (e) {
+//         e.preventDefault();
+//         const id = this.getAttribute("href");
+//         console.log(id);
+//         document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+//     });
+// });
+
+document.querySelector(".nav__links").addEventListener("click", function (e) {
+    e.preventDefault();
+
+    if (e.target.classList.contains("nav__link")) {
+        const id = e.target.getAttribute("href");
+        document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+    }
+});
