eb2560541e6e5a948c728bd0fb78acb9ff468dc3
Diego Alexandre <ddsa.1988@outlook.com>
2022-11-17 14:03:18 -0300

    commit

--- 144-AsynchronousJavascript/script.js
+++ 144-AsynchronousJavascript/script.js
@@ -3,18 +3,9 @@
 const btn = document.querySelector(".btn-country");
 const countriesContainer = document.querySelector(".countries");
 
-function getCountryData(country) {
-    const request = new XMLHttpRequest();
-    request.open("GET", `https://restcountries.com/v2/name/${country}`);
-    request.send();
-
-    request.addEventListener("load", function () {
-        const [data] = JSON.parse(this.responseText);
-
-        console.log(data);
-
-        const html = `
-    <article class="country">
+function renderCountry(data, className = "") {
+    const html = `
+    <article class="country ${className}">
     <img class="country__img" src="${data.flag}" />
         <div class="country__data">
             <h3 class="country__name">${data.name}</h3>
@@ -29,10 +20,41 @@ function getCountryData(country) {
         </div>
     </article>`;
 
-        countriesContainer.insertAdjacentHTML("beforeend", html);
-        countriesContainer.style.opacity = 1;
+    countriesContainer.insertAdjacentHTML("beforeend", html);
+    countriesContainer.style.opacity = 1;
+}
+
+function getCountryData(country) {
+    //AJAX call country 1
+    const request1 = new XMLHttpRequest();
+    request1.open("GET", `https://restcountries.com/v2/name/${country}`);
+    request1.send();
+
+    request1.addEventListener("load", function () {
+        const [data1] = JSON.parse(this.responseText);
+        console.log(JSON.parse(this.responseText));
+        console.log(data1);
+
+        //Render country 1
+        renderCountry(data1);
+
+        //Get neighbour country
+        const neighbour = data1.borders?.[0];
+
+        //AJAX call country 2
+        const request2 = new XMLHttpRequest();
+        request2.open("GET", `https://restcountries.com/v2/alpha/${neighbour}`);
+        request2.send();
+
+        request2.addEventListener("load", function () {
+            const data2 = JSON.parse(this.responseText);
+            console.log(data2);
+
+            //Render country 2
+            renderCountry(data2, "neighbour");
+        });
     });
 }
 
 getCountryData("portugal");
-getCountryData("usa");
+// getCountryData("usa");
